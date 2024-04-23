e0daf2e42cf348b3de256c1d19712c76498682e8
Rodrigo Sakae <devsakae@gmail.com>
2024-04-23 20:12:54 -0300

    firebase pack

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './Components/Hero'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Hero />
       <h2>Associação Sul Catarinense de Karatê</h2>
     </>
   )
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <div className='karate_generico'></div>
+    </section>
+  )
+}
+
+export default Hero
