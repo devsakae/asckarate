aa9348edd189fa854ef4c950a110efaed66d1ee5
Pietro Bucker <pietro_177@hotmail.com>
2024-04-26 22:23:41 -0300

    colocado user entre o provider para recber o context

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home';
-import User from './components/Hero';
-
+import User from './components/User';
+import { FirebaseProvider } from './store/FirebaseProvider';
 function App() {
 
   return (
   <BrowserRouter>
       <Routes>
-          <Route path="/login" element={<User />} />
+          <Route 
+            path="/login" 
+            element={
+            <FirebaseProvider>
+            <User />
+            </FirebaseProvider>
+            } />
           <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
